c2fb2703edae006e0179c1f940aa560ec89145ee
wkdtj <wkdtj@192.168.219.103>
2022-01-06 14:37:58 +0900

    s

--- src/test/overload1/RunOverloadTest1.java
+++ src/test/overload1/RunOverloadTest1.java
@@ -1,21 +1,32 @@
 package test.overload1;
 
+import java.util.Scanner;
 public class RunOverloadTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 		MethodOverloadTest1 ot1 = new MethodOverloadTest1();
 		
-		int n1 = 150;
-		int n2 = 3;
+		System.out.print("* 정수 1 입력: ");
+		int n1 = s.nextInt();
+		System.out.print("* 정수 2 입력: ");
+		int n2 = s.nextInt();
+		
 		
 		System.out.printf("%d*%d = %d\n", n1, n2, ot1.calc(n1, n2));
 		
-		int n3= 30;
-		int n4 = 50;
-		int n5 = 100;
+		System.out.print("* 정수 1 입력: ");
+		int n3= s.nextInt();
+		System.out.print("* 정수 2 입력: ");
+		int n4 = s.nextInt();
+		System.out.print("* 정수 3 입력: ");
+		int n5 = s.nextInt();
+		
 		
 		System.out.printf("%d+%d = %d\n", n3, n4, n5, ot1.calc(n3, n4, n5));
+		
+		s.close();
 		
 	}
 
