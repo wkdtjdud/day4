183887f3f77a7a82472fc51d4a00913947a89449
wkdtj <wkdtj@192.168.219.103>
2022-01-06 15:48:48 +0900

    Con

---
+++ src/test/constructor1/RunConstructorTest1.java
@@ -0,0 +1,16 @@
+package test.constructor1;
+
+public class RunConstructorTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ConstructorTest1 c1 = new ConstructorTest1(7);
+		ConstructorTest1 c2 = new ConstructorTest1(11);
+		
+		
+		System.out.println("c1이 참조하는 객체의 n1 필드의 값은 "+ c1.getN1());
+		System.out.println("c2이 참조하는 객체의 n1 필드의 값은 "+ c2.getN1());
+
+	}
+
+}
